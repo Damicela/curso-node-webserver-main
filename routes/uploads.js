79a25be52f0a2290041fc7ae25cc3400f91e6c41
Damicela Peña <damicelapr@gmail.com>
2023-03-01 18:52:48 -0600

    rest server completado

--- routes/uploads.js
+++ routes/uploads.js
@@ -1,6 +1,6 @@
 const {Router} = require('express');
 const { check } = require('express-validator');
-const { cargarArchivo, actualizarImagen, mostrarImagen } = require('../controllers/uploads');
+const { cargarArchivo, actualizarImagen, mostrarImagen, actualizarImagenCloudinary } = require('../controllers/uploads');
 
 const { validarCampos } = require('../middlewares/validar-campos');
 const {coleccionesPermitidas} = require('../helpers');
@@ -15,7 +15,7 @@ router.put('/:coleccion/:id', [
     check('id', 'El id debe de ser de mongo').isMongoId(),
     check('coleccion').custom(c => coleccionesPermitidas(c, ['usuarios', 'productos'])),
     validarCampos
-], actualizarImagen)
+], actualizarImagenCloudinary)
 
 router.get('/:coleccion/:id', [
   check('id', 'El id debe de ser de mongo').isMongoId(),
